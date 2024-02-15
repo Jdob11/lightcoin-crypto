bda3e25569834da44f20f07826fa3c0fb81067e9
Jdob11 <jbdobson11@gmail.com>
2024-02-15 06:12:15 -0800

    wrote Litecoin Crypto App

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  get balance() {
+    let balance = 0;
+    for (let transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) {
+      return false;
+    };
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
 
+}
+
+class Deposit extends Transaction{
+
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    return true;
+  }
+
+}
+
+class Withdrawal extends Transaction{
+
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
 }
 
 
@@ -18,12 +65,25 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account("cash-money");
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+console.log("Account Balance:", myAccount.balance);
 
-console.log('Balance:', balance);
+console.log("Trying to withdraw $20");
+const t1 = new Withdrawal(20.00, myAccount);
+console.log('Commit: ', t1.commit());
+console.log('Current Balance: ', myAccount.balance);
+
+console.log("Depositing $100");
+const t2 = new Deposit(100.00, myAccount);
+console.log('Commit: ', t2.commit());
+console.log("Current Balance: ", myAccount.balance);
+
+console.log("Withdrawing $50");
+const t3 = new Withdrawal(50.00, myAccount);
+console.log('Commit: ', t3.commit());
+console.log("Final Account Balance", myAccount.balance);
+
+
+console.log('Account Transaction History: ', myAccount.transactions);
+
